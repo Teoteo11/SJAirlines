ae28c853fc8413c75e4b2a9dea60fb6d0d2b8b93
Bazzastyle <bazzastyle@hotmail.it>
2019-11-21 19:31:20 +0100

    add users

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import tickets from "./routes/tickets"
+import users from "./routes/users"
+import companies from "./routes/companies"
 const app = express();
 
 app.use("/tickets",tickets);
+app.use("/users",users);
+app.use("/companies",companies);
 
 app.listen(5000,function(){
     console.log("SERVER RUNNING");
---
+++ src/routes/users.ts
@@ -0,0 +1,8 @@
+import express from "express"
+import bodyParser from "body-parser";
+
+const router = express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
+export = router;
