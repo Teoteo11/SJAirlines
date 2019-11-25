62197eeb2ec16bae58a8002dc68d696ccc2d3e69
dandelionsam <aiellosimone.ct@gmail.com>
2019-11-25 21:39:56 +0100

    Exporting module app added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParse from "body-parser";
-import company from "./routes/companies"
-import ticket from "./routes/tickets"
-import user from "./routes/users"
+import airplaneRouter from "./routes/airplanes"
+import { AirplaneModel } from "./model/airplane"
+import { FlightModel } from "./model/flight"
+import { RouteModel } from "./model/route"
+import { CompanyModel } from "./model/company"
+import { UserModel } from "./model/user"
+import { TicketModel } from "./model/ticket"
 
 const app = express();
 app.use(bodyParse.json());
@@ -20,19 +24,22 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.use("/company", company);
-app.use("/user", user);
-app.use("/tickets", ticket)
+app.use("/airplane", airplaneRouter);
+app.use("/flight", airplaneRouter);
+app.use("/company", airplaneRouter);
+app.use("/user", airplaneRouter);
 
 const address = "mongodb://Gabriele:helloworld@football-shard-00-00-9yxib.mongodb.net:27017,football-shard-00-01-9yxib.mongodb.net:27017,football-shard-00-02-9yxib.mongodb.net:27017/sj-airlines?ssl=true&replicaSet=football-shard-0&authSource=admin&retryWrites=true&w=majority"
 
-app.listen(5000, async () => {
+app.listen(300, async () => {
     await mongoose.connect(address, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => {
-        console.log("Connected successfully!")
+        console.log("Connected successfully!");
     }).catch(error => {
-        console.log("Error connection!")
+        console.log("Error connection!");
     });
 })
+
+module.exports = app;
