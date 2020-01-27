142395870d1a07f752303308845ac9d8d6a44f95
amatoManuel <manuel.amato_2018@libero.it>
2020-01-27 17:22:58 +0100

    fix flights.ts

--- src/controllers/flights.ts
+++ src/controllers/flights.ts
@@ -1,25 +1,60 @@
-import express, { Request, Response } from "express";
-import bodyParser from "body-parser";
+import { Request, Response } from "express";
 
 import { FlightModel } from "../model/flight";
-
+import { Flight, Airplane } from "..";
+import { AirplaneModel } from "../model/airplane";
+import { validationResult } from "express-validator";
 
 export const getFlights = async (req: Request, res: Response) => {
   if (req.query.id) {
     try {
       const flight = await FlightModel.findById(req.query.id);
       return res.status(200).json(flight);
-    } 
-    catch (error) {
+    } catch (error) {
       return res.status(500).json({ message: error });
     }
   } else {
     try {
       const flights = await FlightModel.find();
       return res.status(200).json(flights);
-    } 
-    catch (error) {
+    } catch (error) {
       return res.status(500).json({ message: error });
     }
   }
-}
+};
+
+export const getFilteredFlights = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: "Unprocessable entity" });
+  }
+  try {
+    let flights: Array<Flight>;
+    if (req.query.checkOut && req.query.destination) {
+      flights = (await FlightModel.find({
+        departure: req.params.idAirport,
+        checkIn: req.query.checkIn,
+        destination: req.query.destination,
+        checkOut: req.query.checkOut
+      })) as any;
+    } else {
+      flights = (await FlightModel.find({
+        departure: req.params.idAirport,
+        checkIn: req.query.checkIn
+      })) as any;
+      flights = flights.filter(async flight => {
+        const airplane: Airplane = (await AirplaneModel.findById(
+          flight.idAirplane
+        )) as any;
+        if (Number(airplane.numSeats) - Number(req.params.nSeats) < 0) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      return res.status(200).json(flights);
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
--- src/routes/flights.ts
+++ src/routes/flights.ts
@@ -1,9 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { FlightModel } from "../model/flight";
-import { check, query } from "express-validator";
-import { Flight, Airplane } from "../index";
-import { AirplaneModel } from "../model/airplane";
+import { param, query } from "express-validator";
 import * as FlightsController from "../controllers/flights";
 
 const router = express.Router();
@@ -14,40 +10,15 @@ const router = express.Router();
 router.get("/", FlightsController.getFlights);
 
 // dargli un senso
-router.get("/:idAirport/:nSeats", async (req, res) => {
-  try {
-    if (req.params.idAirport && req.params.nSeats) {
-      let flights: Array<Flight>;
-      if (req.query.checkOut && req.query.destination) {
-        flights = (await FlightModel.find({
-          departure: req.params.idAirport,
-          checkIn: req.query.checkIn,
-          destination: req.query.destination,
-          checkOut: req.query.checkOut
-        })) as any;
-      } else {
-        flights = (await FlightModel.find({
-          departure: req.params.idAirport,
-          checkIn: req.query.checkIn
-        })) as any;
-      }
-      res.status(200).json(
-        flights.filter(async flight => {
-          const airplane: Airplane = (await AirplaneModel.findById(
-            flight.idAirplane
-          )) as any;
-          if (Number(airplane.numSeats) - Number(req.params.nSeats) < 0) {
-            return false;
-          } else {
-            return true;
-          }
-        })
-      );
-    }
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-  return;
-});
+router.get(
+  "/:idAirport/:nSeats",
+  [
+    param("idAirport").isMongoId(),
+    param("nSeats").isNumeric(),
+    query(["checkOut", "checkIn"]).isISO8601(),
+    query("destination").isString()
+  ],
+  FlightsController.getFilteredFlights
+);
 
 export = router;
